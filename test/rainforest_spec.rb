ce44c957f926d1fc72866979de4b1ad6202a604f
Welly Salim <welly@Wellys-MacBook-Pro.local>
2023-07-03 08:39:59 +1000

    reduce duplication

--- test/rainforest_spec.rb
+++ test/rainforest_spec.rb
@@ -2,34 +2,34 @@ require 'webmock/rspec'
 require_relative '../rainforest'
 
 SAMPLE_URL = 'https://www.letsrevolutionizetesting.com/challenge?id='
+NOT_END_MESSAGE = 'This is not the end'
+END_MESSAGE = 'Congratulations! You\'ve reached the end! You have passed our simple test and we would love to hear from you. Please save the code you used to a private GitHub gist and fill out the application form at https://jobs.lever.co/rainforest with the link to the gist. We\'ll be in touch shortly!'
+
 RSpec.configure do |config|
   config.before(:each) do
-
     stub_request(:get, "#{SAMPLE_URL}123").
     to_return(status: 200, body: response = {
-      message: 'This is not the end',
+      message: NOT_END_MESSAGE,
       follow: "#{SAMPLE_URL}456"
     }.to_json)
 
     stub_request(:get, "#{SAMPLE_URL}456").
     to_return(status: 200, body: response = {
-      message: 'This is not the end',
+      message: NOT_END_MESSAGE,
       follow: "#{SAMPLE_URL}789"
     }.to_json)
 
     stub_request(:get, "#{SAMPLE_URL}789").
     to_return(status: 200, body: response = {
-      message: 'Congratulations! You\'ve reached the end! You have passed our simple test and we would love to hear from you. Please save the code you used to a private GitHub gist and fill out the application form at https://jobs.lever.co/rainforest with the link to the gist. We\'ll be in touch shortly!'
+      message: END_MESSAGE
     }.to_json)
-
-
   end
 end
 
 describe '#run' do
   let(:response) { run("#{SAMPLE_URL}123") }
   it 'return end message' do
-    expect(response.message).not_to eq('This is not the end')
-    expect(response.message).to eq('Congratulations! You\'ve reached the end! You have passed our simple test and we would love to hear from you. Please save the code you used to a private GitHub gist and fill out the application form at https://jobs.lever.co/rainforest with the link to the gist. We\'ll be in touch shortly!')
+    expect(response.message).not_to eq(NOT_END_MESSAGE)
+    expect(response.message).to eq(END_MESSAGE)
   end
 end
